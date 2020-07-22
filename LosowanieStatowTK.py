3dcdd794093794f815b2ca8aca33ea9a12c84eac
CrazyHamster27 <pawel.socha27@gmail.com>
2020-07-22 16:45:16 +0200

    Dodano operacje na statystykach w zakresie sumy punktów

--- LosowanieStatowTK.py
+++ LosowanieStatowTK.py
@@ -10,8 +10,9 @@ sum = 0
 rest = 0
 
 def losuj():
-    sum = 0
+    global rest
     rest = 0
+    sum = 0
     while sum < 70:
         sum = 0
         for i in range(0,6):
@@ -25,31 +26,61 @@ def losuj():
     label_int_val = Label(root, text=val[3])
     label_wis_val = Label(root, text=val[4])
     label_cha_val = Label(root, text=val[5])
-    label_str.grid(row=1, column=0)
     label_str_val.grid(row=1, column=2)
-    label_dex.grid(row=2, column=0)
     label_dex_val.grid(row=2, column=2)
-    label_con.grid(row=3, column=0)
     label_con_val.grid(row=3, column=2)
-    label_int.grid(row=4, column=0)
     label_int_val.grid(row=4, column=2)
-    label_wis.grid(row=5, column=0)
     label_wis_val.grid(row=5, column=2)
-    label_cha.grid(row=6, column=0)
     label_cha_val.grid(row=6, column=2)
     label_sum_val = Label(root, text=sum)
     label_sum_val.grid(row=7, column=2)
+    label_rest_val = Label(root, text=rest)
+    label_rest_val.grid(row=9, column=2)
 
 
 def add(x):
-    if rest > 0 and x < 18:
-        x = x + 1
+    global rest
+    if 18 > val[x] and rest > 0:
+        val[x] = val[x] + 1
         rest = rest - 1
 
+    label_str_val = Label(root, text=val[0])
+    label_dex_val = Label(root, text=val[1])
+    label_con_val = Label(root, text=val[2])
+    label_int_val = Label(root, text=val[3])
+    label_wis_val = Label(root, text=val[4])
+    label_cha_val = Label(root, text=val[5])
+    label_rest_val = Label(root, text=rest)
+    label_str_val.grid(row=1, column=2)
+    label_dex_val.grid(row=2, column=2)
+    label_con_val.grid(row=3, column=2)
+    label_int_val.grid(row=4, column=2)
+    label_wis_val.grid(row=5, column=2)
+    label_cha_val.grid(row=6, column=2)
+    label_rest_val.grid(row=9, column=2)
+
+
+
 def sub(x):
-    if x > 3:
-        x = x - 1
+    global rest
+    if val[x] > 3 and rest >= 0:
+        val[x] = val[x] - 1
         rest = rest + 1
+
+    label_str_val = Label(root, text=val[0])
+    label_dex_val = Label(root, text=val[1])
+    label_con_val = Label(root, text=val[2])
+    label_int_val = Label(root, text=val[3])
+    label_wis_val = Label(root, text=val[4])
+    label_cha_val = Label(root, text=val[5])
+    label_rest_val = Label(root, text=rest)
+    label_str_val.grid(row=1, column=2)
+    label_dex_val.grid(row=2, column=2)
+    label_con_val.grid(row=3, column=2)
+    label_int_val.grid(row=4, column=2)
+    label_wis_val.grid(row=5, column=2)
+    label_cha_val.grid(row=6, column=2)
+    label_rest_val.grid(row=9, column=2)
 
 
 # Labele
@@ -92,18 +123,18 @@ label_rest.grid(row=9, column=0)
 label_rest_val.grid(row=9, column=2)
 
 # Buttons
-sub_str = Button(root, text='-', command=None, state=DISABLED)
-add_str = Button(root, text='+', command=None, state=DISABLED)
-sub_dex = Button(root, text='-', command=None, state=DISABLED)
-add_dex = Button(root, text='+', command=None, state=DISABLED)
-sub_con = Button(root, text='-', command=None, state=DISABLED)
-add_con = Button(root, text='+', command=None, state=DISABLED)
-sub_int = Button(root, text='-', command=None, state=DISABLED)
-add_int = Button(root, text='+', command=None, state=DISABLED)
-sub_wis = Button(root, text='-', command=None, state=DISABLED)
-add_wis = Button(root, text='+', command=None, state=DISABLED)
-sub_cha = Button(root, text='-', command=None, state=DISABLED)
-add_cha = Button(root, text='+', command=None, state=DISABLED)
+sub_str = Button(root, text='-', command=lambda: sub(0))
+add_str = Button(root, text='+', command=lambda: add(0))
+sub_dex = Button(root, text='-', command=lambda: sub(1))
+add_dex = Button(root, text='+', command=lambda: add(1))
+sub_con = Button(root, text='-', command=lambda: sub(2))
+add_con = Button(root, text='+', command=lambda: add(2))
+sub_int = Button(root, text='-', command=lambda: sub(3))
+add_int = Button(root, text='+', command=lambda: add(3))
+sub_wis = Button(root, text='-', command=lambda: sub(4))
+add_wis = Button(root, text='+', command=lambda: add(4))
+sub_cha = Button(root, text='-', command=lambda: sub(5))
+add_cha = Button(root, text='+', command=lambda: add(5))
 
 losuj_btn = Button(root, text='Losuj', command=losuj)
 
