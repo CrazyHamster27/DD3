a945719c560f8c64067bf167652243c808eb46ad
CrazyHamster27 <pawel.socha27@gmail.com>
2020-07-25 11:16:43 +0200

    Modyfikacja graficzna guzikow i rozmiaru okna, dodana funkcja zapisz/wczytaj(to trzeba poprawic) poprzedni wynik

--- LosowanieStatowTK.py
+++ LosowanieStatowTK.py
@@ -4,13 +4,16 @@ from random import randint
 
 root = Tk()
 root.title('Losowanie Statystyk')
-root.geometry('400x400')
+root.geometry('150x300')
 val = [0, 0, 0, 0, 0, 0]
+remember_throw = []
 sum = 0
 rest = 0
 
+
 def losuj():
     global rest
+    global sum
     rest = 0
     sum = 0
     while sum < 65:
@@ -35,7 +38,7 @@ def losuj():
     label_wis_val.grid(row=5, column=2)
     label_cha_val.grid(row=6, column=2)
     label_sum_val.grid(row=7, column=2)
-    label_rest_val.grid(row=9, column=2)
+    label_rest_val.grid(row=8, column=2)
 
 
 def add(x):
@@ -57,7 +60,7 @@ def add(x):
     label_int_val.grid(row=4, column=2)
     label_wis_val.grid(row=5, column=2)
     label_cha_val.grid(row=6, column=2)
-    label_rest_val.grid(row=9, column=2)
+    label_rest_val.grid(row=8, column=2)
 
 
 
@@ -80,7 +83,42 @@ def sub(x):
     label_int_val.grid(row=4, column=2)
     label_wis_val.grid(row=5, column=2)
     label_cha_val.grid(row=6, column=2)
-    label_rest_val.grid(row=9, column=2)
+    label_rest_val.grid(row=8, column=2)
+
+
+def remember():
+    global remember_throw
+    global last_sum
+    global last_rest
+    remember_throw = val.copy()
+    print(remember_throw)
+    last_sum = sum
+    print(last_sum)
+    last_rest = rest
+    print(rest)
+
+
+def load():
+    val = remember_throw.copy()
+    sum = last_sum
+    rest = last_rest
+
+    label_str_val = Label(root, text=val[0])
+    label_dex_val = Label(root, text=val[1])
+    label_con_val = Label(root, text=val[2])
+    label_int_val = Label(root, text=val[3])
+    label_wis_val = Label(root, text=val[4])
+    label_cha_val = Label(root, text=val[5])
+    label_sum_val = Label(root, text=sum)
+    label_rest_val = Label(root, text=rest)
+    label_str_val.grid(row=1, column=2)
+    label_dex_val.grid(row=2, column=2)
+    label_con_val.grid(row=3, column=2)
+    label_int_val.grid(row=4, column=2)
+    label_wis_val.grid(row=5, column=2)
+    label_cha_val.grid(row=6, column=2)
+    label_sum_val.grid(row=7, column=2)
+    label_rest_val.grid(row=8, column=2)
 
 
 # Labele
@@ -119,24 +157,27 @@ label_cha.grid(row=6, column=0)
 label_cha_val.grid(row=6, column=2)
 label_sum.grid(row=7, column=0)
 label_sum_val.grid(row=7, column=2)
-label_rest.grid(row=9, column=0)
-label_rest_val.grid(row=9, column=2)
+label_rest.grid(row=8, column=0)
+label_rest_val.grid(row=8, column=2)
 
 # Buttons
-sub_str = Button(root, text='-', command=lambda: sub(0))
-add_str = Button(root, text='+', command=lambda: add(0))
-sub_dex = Button(root, text='-', command=lambda: sub(1))
-add_dex = Button(root, text='+', command=lambda: add(1))
-sub_con = Button(root, text='-', command=lambda: sub(2))
-add_con = Button(root, text='+', command=lambda: add(2))
-sub_int = Button(root, text='-', command=lambda: sub(3))
-add_int = Button(root, text='+', command=lambda: add(3))
-sub_wis = Button(root, text='-', command=lambda: sub(4))
-add_wis = Button(root, text='+', command=lambda: add(4))
-sub_cha = Button(root, text='-', command=lambda: sub(5))
-add_cha = Button(root, text='+', command=lambda: add(5))
+sub_str = Button(root, text='-', command=lambda: sub(0), width=3)
+add_str = Button(root, text='+', command=lambda: add(0), width=3)
+sub_dex = Button(root, text='-', command=lambda: sub(1), width=3)
+add_dex = Button(root, text='+', command=lambda: add(1), width=3)
+sub_con = Button(root, text='-', command=lambda: sub(2), width=3)
+add_con = Button(root, text='+', command=lambda: add(2), width=3)
+sub_int = Button(root, text='-', command=lambda: sub(3), width=3)
+add_int = Button(root, text='+', command=lambda: add(3), width=3)
+sub_wis = Button(root, text='-', command=lambda: sub(4), width=3)
+add_wis = Button(root, text='+', command=lambda: add(4), width=3)
+sub_cha = Button(root, text='-', command=lambda: sub(5), width=3)
+add_cha = Button(root, text='+', command=lambda: add(5), width=3)
 
 losuj_btn = Button(root, text='Losuj', command=losuj)
+
+remember_btn = Button(root, text='Zapamiętaj', command=remember)
+load_btn = Button(root, text='Wczytaj', command=load)
 
 # Grid - Buttons
 sub_str.grid(row=1, column=1)
@@ -151,7 +192,9 @@ sub_wis.grid(row=5, column=1)
 add_wis.grid(row=5, column=3)
 sub_cha.grid(row=6, column=1)
 add_cha.grid(row=6, column=3)
-losuj_btn.grid(row=8, columnspan=4)
+losuj_btn.grid(row=9, columnspan=4, sticky=W + E)
+remember_btn.grid(row=10, column=0, columnspan=2, sticky=W+E)
+load_btn.grid(row=10, column=2, columnspan=2, sticky=W+E)
 
 
 # Mainloop
