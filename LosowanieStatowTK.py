ef932fcf1fe9bd72e861002afb5c174bf708521e
CrazyHamster27 <pawel.socha27@gmail.com>
2020-07-26 10:23:57 +0200

    Dodano opcje zapisywanie wyniku losownia do pliku .txt, drobne poprawki w labelsach

--- LosowanieStatowTK.py
+++ LosowanieStatowTK.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from random import randint
+from tkinter import filedialog
 
-# tkinter window
+# Main window
 root = Tk()
 root.title('Losowanie Statystyk')
 root.geometry('250x300')
 
 # lists
 attributes_list = ["Siła", "Zręczność", "Kondycja", "Inteligencja", "Mądrość", "Charyzma"]
-btn_list = ["+", '-', "Losuj!", "Zapamiętaj", "Wczytaj"]
+labels_list=['Twoje Statystyki:', "Suma:", "Pozostało:"]
+btn_list = ["+", '-', "Losuj!", "Zapamiętaj", "Wczytaj", "Zapisz w Pliku"]
 
 # starting values
 attributes_values_list = [0, 0, 0, 0, 0, 0]
@@ -19,11 +21,12 @@ rest = 0
 
 # Labels - def
 def attributes_labels():
-    Label(root, text='Twoje Statystyki').grid(row=0, columnspan=4)
+    Label(root, text=labels_list[0]).grid(row=0, columnspan=4)
+    Label(root, text=labels_list[1]).grid(row=7, column=0)
+    Label(root, text=labels_list[2]).grid(row=8, column=0)
     for i in range(6):
         Label(root, text=attributes_list[i], width=15).grid(row=i+1, column=0)
-    Label(root, text='Suma:').grid(row=7, column=0)
-    Label(root, text='Pozostało:').grid(row=8, column=0)
+
 
 
 # Values - def
@@ -63,6 +66,7 @@ def buttons():
     Button(root, text=btn_list[2], command=losuj).grid(row=9, columnspan=4, sticky=W + E)
     Button(root, text=btn_list[3], command=remember).grid(row=10, column=0, columnspan=2, sticky=W + E)
     Button(root, text=btn_list[4], command=load).grid(row=10, column=2, columnspan=2, sticky=W + E)
+    Button(root, text=btn_list[5], command=file_save).grid(row=11, column=0, columnspan=4, sticky=W + E)
 
 
 def losuj():
@@ -100,6 +104,15 @@ def load():
 
     attributes_labels()
     attributes_values()
+
+
+def file_save():
+    file = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+    file.write(str(labels_list[0])+"\n\n")
+    for i in range(6):
+        file.write(str(attributes_list[i]) + ' ' + str(attributes_values_list[i]) + '\n')
+    file.write('\n' + str(labels_list[1]) + ' ' + str(sum))
+    file.close()
 
 
 # Labele
